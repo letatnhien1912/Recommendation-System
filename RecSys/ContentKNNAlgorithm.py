98705d43ea514ae726be6bcdcf721a3de444cb08
letatnhien1912@gmail.com <letatnhien1912@gmail.com>
2024-09-08 16:33:23 +0700

    intiate recommendation system project

---
+++ RecSys/ContentKNNAlgorithm.py
@@ -0,0 +1,82 @@
+from surprise import AlgoBase
+from surprise import PredictionImpossible
+from RecSys.ProductPreference import ProductInfo
+import math
+import numpy as np
+import heapq
+
+class ContentKNNAlgorithm(AlgoBase):
+
+    def __init__(self, k=40, sim_options={}):
+        AlgoBase.__init__(self)
+        self.k = k
+
+    def fit(self, trainset):
+        AlgoBase.fit(self, trainset)
+
+        # Compute item similarity matrix based on content attributes
+
+        # Load up genre vectors for every product
+        ml = ProductInfo()
+        tags = ml.getTags()
+        
+        print("Computing content-based similarity matrix...")
+            
+        # Compute genre distance for every product combination as a 2x2 matrix
+        self.similarities = np.zeros((self.trainset.n_items, self.trainset.n_items))
+        
+        for thisRating in range(self.trainset.n_items):
+            if (thisRating % 100 == 0):
+                print(thisRating, " of ", self.trainset.n_items)
+            for otherRating in range(thisRating+1, self.trainset.n_items):
+                thisProductID = int(self.trainset.to_raw_iid(thisRating))
+                otherProductID = int(self.trainset.to_raw_iid(otherRating))
+                genreSimilarity = self.computeTagSimilarity(thisProductID, otherProductID, tags)
+                self.similarities[thisRating, otherRating] = genreSimilarity
+                self.similarities[otherRating, thisRating] = self.similarities[thisRating, otherRating]
+                
+        print("...done.")
+                
+        return self
+    
+    def computeTagSimilarity(self, product1, product2, tags):
+        tags1 = tags[product1]
+        tags2 = tags[product2]
+
+        sumxx, sumxy, sumyy = 0, 0, 0
+        for i in range(len(tags1)):
+            x = tags1[i]
+            y = tags2[i]
+            sumxx += x * x
+            sumyy += y * y
+            sumxy += x * y
+        
+        return sumxy/math.sqrt(sumxx*sumyy)
+
+    def estimate(self, u, i):
+
+        if not (self.trainset.knows_user(u) and self.trainset.knows_item(i)):
+            raise PredictionImpossible('User and/or item is unkown.')
+        
+        # Build up similarity scores between this item and everything the user rated
+        neighbors = []
+        for rating in self.trainset.ur[u]:
+            genreSimilarity = self.similarities[i,rating[0]]
+            neighbors.append( (genreSimilarity, rating[1]) )
+        
+        # Extract the top-K most-similar ratings
+        k_neighbors = heapq.nlargest(self.k, neighbors, key=lambda t: t[0])
+        
+        # Compute average sim score of K neighbors weighted by user ratings
+        simTotal = weightedSum = 0
+        for (simScore, rating) in k_neighbors:
+            if (simScore > 0):
+                simTotal += simScore
+                weightedSum += simScore * rating
+            
+        if (simTotal == 0):
+            raise PredictionImpossible('No neighbors')
+
+        predictedRating = weightedSum / simTotal
+
+        return predictedRating
